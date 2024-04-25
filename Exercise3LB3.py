05acf8755f73574622e38c5b1ef6bbf8ce546888
RineDall <s299877@studenti.polito.it>
2024-04-25 15:29:12 +0200

    second commit

--- Exercise3LB3.py
+++ Exercise3LB3.py
@@ -1,67 +1,65 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.colors as mcol
-
-color_dict = mcol.TABLEAU_COLORS
 
 class ADC:
     fs_step = 2.75625e3
 
     @staticmethod
-    def sampling_frequency(analog_bandwidth):
-        return np.ceil(analog_bandwidth / ADC.fs_step) * ADC.fs_step
+    def sampling_freq(analog_band):
+        return np.ceil(analog_band / ADC.fs_step) * ADC.fs_step
 
-    def __init__(self, n_bit):
-        self.n_bit = n_bit
+    def __init__(s, n_bits):
+        s.n_bit = n_bits
 
-    def snr(self):
-        return 6 * self.n_bit
+    def snr(s):
+        return 6 * s.n_bit
 
 class BSC:
-    def __init__(self, error_probability):
-        self.error_probability = error_probability
+    def __init__(s, error_prob):
+        s.error_prob = error_prob
 
-    def can_support(self, n_bit):
-        return self.error_probability <= 1 / (2 ** n_bit)
+    def can_transmit(s, n_bit):
+        pe_tresh = (1 / (4 * (2 ** (2 * n_bit) - 1)))
+        print("Pe threshold", pe_tresh)
+        return s.error_prob <= pe_tresh
 
 class PCM:
-    def __init__(self, analog_bandwidth, adc_n_bit, bsc_error_probability):
-        self.analog_bandwidth = analog_bandwidth
-        self.adc = ADC(adc_n_bit)
-        self.bsc = BSC(bsc_error_probability)
+    def __init__(s, analog_band, adc_n_bit, bsc_error_prob):
+        s.analog_band = analog_band
+        s.adc = ADC(adc_n_bit)
+        s.bsc = BSC(bsc_error_prob)
 
-    def effective_quantization_snr(self):
-        return self.adc.snr()
+    def effective_quant_snr(s):
+        return s.adc.snr()
 
 # Given data
-analog_bandwidth = 22e3  # 22 KHz
-quantization_snr_required = 80  # dB
-bsc_error_probability = 3.8e-7
+analog_band = 22e3  # 22 KHz
+quant_snr_required = 80  # dB
+bsc_error_prob = 3.8e-7
 
 print("Given Data:")
-print("Analog Bandwidth:", analog_bandwidth)
-print("Quantization SNR Required:", quantization_snr_required, "dB")
-print("BSC Error Probability:", bsc_error_probability)
+print("Analog Bandwidth:", analog_band)
+print("Quantization SNR Required:", quant_snr_required, "dB")
+print("BSC Error Probability:", bsc_error_prob)
 
-# Calculate ADC number of bits
-n_bit = np.ceil(quantization_snr_required / 6).astype(int)
+# ADC number of bits
+n_bit = np.ceil(quant_snr_required / 6).astype(int)
 
-print("\nCalculated ADC Number of Bits:", n_bit)
+print("\n ADC Number of Bits:", n_bit)
 
-# Calculate required ADC sampling frequency
-adc_sampling_frequency = ADC.sampling_frequency(analog_bandwidth)
+# Calculating sampling freq ADC
+adc_sampling_freq = ADC.sampling_freq(analog_band)
 
-print("Required ADC Sampling Frequency:", adc_sampling_frequency, "Hz")
+print("Required Sampling Frequency ADC:", adc_sampling_freq, "Hz")
 
-# Create PCM object
-pcm = PCM(analog_bandwidth, n_bit, bsc_error_probability)
+# Creating PCM obj
+pcm = PCM(analog_band, n_bit, bsc_error_prob)
 
-# Calculate effective quantization SNR
-effective_quantization_snr = pcm.effective_quantization_snr()
+# Calculating effective quantization SNR
+effective_quant_snr = pcm.effective_quant_snr()
 
-print("Effective Quantization SNR:", effective_quantization_snr, "dB")
+print("SNR Effective Quantization:", effective_quant_snr, "dB")
 
-# Check if BSC can support transmission
-can_support = pcm.bsc.can_support(n_bit)
+# Check if BSC can succesfully transmit
+can_support = pcm.bsc.can_transmit(n_bit)
 
-print("Can BSC Support Transmission with the Given Number of Bits:", can_support)
+print("Can BSC with the given nr of bits transmit:", can_support)
